c36ae414856dd5eef61e11e2891a5084307daf00
cqyll <97649063+cqyll@users.noreply.github.com>
2022-03-15 22:06:19 -0400

    * MainActivity test update * Night theme fix -- MS 6601

--- app/src/main/java/com/example/gryphus/MainActivity.java
+++ app/src/main/java/com/example/gryphus/MainActivity.java
@@ -1,4 +1,27 @@
 package com.example.gryphus;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //removed default action bar, use @toolbar as default action bar
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); //sets `toolbar` as actionbar
+    }
+
+
 }
