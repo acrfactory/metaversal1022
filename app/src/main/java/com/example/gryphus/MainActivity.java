f249024b489bc9be0b962c05306787f8413db174
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2022-03-17 18:30:49 -0400

    2

--- app/src/main/java/com/example/gryphus/MainActivity.java
+++ app/src/main/java/com/example/gryphus/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,9 +22,26 @@ public class MainActivity extends AppCompatActivity {
         //removed default action bar, use @toolbar as default action bar
 
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar); //sets `toolbar` as actionbar
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar); //sets `toolbar` as actionbar
+
+
     }
+
+    private  String getContentOfEditText(int id){
+        View view = findViewById(id);
+        EditText editText = (EditText) view;
+        String str = editText.getText().toString();
+        return str;
+    }//get String value from view
+
+
+        public void login (View view) {
+            LayoutInflater logInflater = getLayoutInflater();
+            View view1 = logInflater.inflate(R.layout.login,null);
+            setContentView(view1);
+        }
+
 
 
 }
