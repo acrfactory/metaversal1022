a25899be3e997652a936851c1fbeeaaf401faef2
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2022-04-02 09:25:39 -0400

    2022-04-02

--- app/src/main/java/com/example/gryphus/AccountModel.java
+++ app/src/main/java/com/example/gryphus/AccountModel.java
@@ -1,62 +1,38 @@
 package com.example.gryphus;
-import static com.google.android.material.internal.ContextUtils.getActivity;
 
-import android.app.Activity;
-import android.view.View;
-import android.widget.EditText;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccountModel {
-    public static ArrayList<String> name = new ArrayList<>(Arrays.asList("James"));
-    public static ArrayList<String> username = new ArrayList<String>(Arrays.asList("abcd1234"));
-    public static ArrayList<String> password = new ArrayList<>(Arrays.asList("abcd1234"));
+    HashMap<String, String> accountMap= new HashMap<>();
 
-    public boolean valid = false;//check whether or not userName, or passWord is valid.
+    public boolean valid = false;//check whether or not username, or password is valid.
 
-    public boolean login (String userName, String passWord) {
-        boolean result = false;
-
-
-        if(username.contains(userName)) {
-            result = true;
-        }
-        if(password.contains(passWord)) {
-            result = true;
-        }
-
-        return result;
+    public void addAccount(String username, String password) {
+        accountMap.put(username, password);
     }
 
-    public void signUp (String name, String userName, String passWord) {
-
-
-        String returnValue;
-
-        if (!username.contains(userName)) {
-            valid = true;
-        }
-
-        //check whether userName is overlapping.
-
-        //check whether passWord is overlapping.
-        if (valid) {
-            this.name.add(name);
-            this.username.add(userName);
-            this.password.add(passWord);
-            returnValue = "Success";
-        } else {
-            returnValue = "Fail";
-        }
-
+    public boolean userExistence(String username){
+        return accountMap.containsKey(username);
     }
 
-    public  String getContentOfEditText(int id, Activity activity){
-        View view = getActivity(activity).getApplicationContext().findViewById(id);
-        EditText editText = (EditText) view;
-        String str = editText.getText().toString();
-        return str;
+    public boolean valid(String username, String password) {
+
+        // Checks for username existence by using `userExistence` method.
+        if(userExistence(username)) {
+            // Checks if password matches username input
+            return password.equals(accountMap.get(username));
+            }
+        return false;
     }
+
+    public void loadAccounts(Map<String, ?> preferencesMap){
+        for (Map.Entry<String, ?> entries: preferencesMap.entrySet()){
+            accountMap.put(entries.getKey(), entries.getValue().toString());
+        }
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/gryphus/ProductFragment.java
+++ app/src/main/java/com/example/gryphus/ProductFragment.java
@@ -1,15 +1,24 @@
 package com.example.gryphus;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Objects;
 
 public class ProductFragment extends Fragment {
-
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedPreferencesEditor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,4 +26,59 @@ public class ProductFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_product, container, false);
     }
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        sharedPreferences = Objects.requireNonNull(getActivity()).getApplicationContext()
+//                .getSharedPreferences("accountDB", Context.MODE_PRIVATE);
+//
+//        sharedPreferencesEditor = sharedPreferences.edit();
+//
+//
+//        Button move1 = getView().findViewById(R.id.list1);
+//        move1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ImageView prd1 = getView().findViewById(R.id.product1);
+//                ImageView prd2 = getView().findViewById(R.id.product2);
+//                ImageView prd3 = getView().findViewById(R.id.product3);
+//                ImageView prd4 = getView().findViewById(R.id.product4);
+//                prd1.setImageResource();
+//                prd2.setImageResource();
+//                prd3.setImageResource();
+//                prd4.setImageResource();
+//            }
+//        });
+//
+//        Button move2 = getView().findViewById(R.id.list2);
+//        move1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ImageView prd1 = getView().findViewById(R.id.product1);
+//                ImageView prd2 = getView().findViewById(R.id.product2);
+//                ImageView prd3 = getView().findViewById(R.id.product3);
+//                ImageView prd4 = getView().findViewById(R.id.product4);
+//                prd1.setImageResource();
+//                prd2.setImageResource();
+//                prd3.setImageResource();
+//                prd4.setImageResource();
+//            }
+//        });
+//
+//        Button move3 = getView().findViewById(R.id.list3);
+//        move1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ImageView prd1 = getView().findViewById(R.id.product1);
+//                ImageView prd2 = getView().findViewById(R.id.product2);
+//                ImageView prd3 = getView().findViewById(R.id.product3);
+//                ImageView prd4 = getView().findViewById(R.id.product4);
+//                prd1.setImageResource();
+//                prd2.setImageResource();
+//                prd3.setImageResource();
+//                prd4.setImageResource();
+//            }
+//        });
+//    }
 }
